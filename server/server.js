16b5cce1387bf178dcc4b9955aec455cd7cbd233
Adrian Ortiz <147290741+Agerian@users.noreply.github.com>
2024-03-24 17:09:04 -0600

    Remove REST API routes

--- server/server.js
+++ server/server.js
@@ -1,11 +1,17 @@
+// Importing modules
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
 
+// GraphQL will replace existing routes
+// const routes = require('./routes'); 
+
+// Creating an express server
 const app = express();
+// Setting the port for production and development
 const PORT = process.env.PORT || 3001;
 
+// Middleware for parsing JSON and urlencoded form data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -14,8 +20,10 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// Middleware for routes
+// app.use(routes);
 
+// Connects to the database and listens on designated port
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
