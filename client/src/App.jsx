0f714fefd0e7d5c5d03228678b3b0e95308ff49c
Adrian Ortiz <147290741+Agerian@users.noreply.github.com>
2024-03-26 02:16:22 -0600

    Add ApolloClient and ApolloProvider to App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,29 @@ import { Outlet } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 
+// Import required components and hooks
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+
+// Create an httpLink to connect to the server
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3001/graphql',
+});
+
+// Instantiate ApolloClient
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
+// Wrap the App component in ApolloProvider
 function App() {
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
+    <ApolloProvider client={client}>
+      <>
+        <Navbar />
+        <Outlet />
+      </>
+    </ApolloProvider>
   );
 }
 
