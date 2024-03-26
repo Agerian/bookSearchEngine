c446d772a49ad9660891b01800170b063d6d5a9c
Adrian Ortiz <147290741+Agerian@users.noreply.github.com>
2024-03-25 19:47:50 -0600

    Fix typo in bookCount field and update parameter type in removeBook mutation

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -5,12 +5,12 @@ const typeDefs = gql`
     _id: ID!
     username: String!
     email: String!
-    bookCouont: Int
+    bookCount: Int
     savedBooks: [Book]
   }
 
   type Book {
-    bookId: ID!
+    bookId: String
     authors: [String]
     description: String
     title: String!
@@ -38,8 +38,10 @@ const typeDefs = gql`
 
   type Mutation {
     login(email: String!, password: String!): Auth
-    addUser(username: String!, email: String!, passowrd: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
     saveBook(input: BookInput!): User
-    removeBook(bookId: String!): User
+    removeBook(bookId: ID!): User
   }
 `;
+
+module.exports = typeDefs;
